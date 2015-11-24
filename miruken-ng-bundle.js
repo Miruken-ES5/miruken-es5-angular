1feec9d9a32cc0ecdc333727754b56869699f7e5
Craig Neuwirt <cneuwirt@gmail.com>
2015-11-24 15:41:46 -0600

    Updated to version 0.0.26

--- miruken-ng-bundle.js
+++ miruken-ng-bundle.js
@@ -353,7 +353,7 @@ new function () { // closure
                     _installPackage(package, module, exports, $injector, runners, starters);
                 }]);
                 module.run(['$rootScope', '$injector', '$q', '$log', function ($rootScope, $injector, $q, $log) {
-                    if (package.parent === base2) {
+                    if (package.parent === base2 && !(package.name in $rootScope)) {
                         $rootScope[package.name] = package;
                     }
                    _provideInjector(appContainer, $injector);
@@ -880,6 +880,7 @@ var Package = Base.extend({
       if (value && value.ancestorOf == Base.ancestorOf && name != "constructor") { // it's a class
         value.toString = K("[" + String2.slice(this, 1, -1) + "." + name + "]");
       }
+      if (this.exported) this.exported([name]);
     }
   },
 
@@ -6840,7 +6841,7 @@ new function () { // closure
      */
     base2.package(this, {
         name:    "miruken",
-        version: "0.0.25",
+        version: "0.0.26",
         exports: "Enum,Variance,Protocol,StrictProtocol,Delegate,Miruken,MetaStep,MetaMacro,Initializing,Disposing,DisposingMixin,Invoking,Parenting,Starting,Startup,Facet,Interceptor,InterceptorSelector,ProxyBuilder,Modifier,ArrayManager,IndexedList,$isProtocol,$isClass,$classOf,$ancestorOf,$isString,$isFunction,$isObject,$isArray,$isPromise,$isNothing,$isSomething,$using,$lift,$equals,$decorator,$decorate,$decorated,$debounce,$eq,$use,$copy,$lazy,$eval,$every,$child,$optional,$promise,$instant,$createModifier,$properties,$inferProperties,$inheritStatic"
     });
 
